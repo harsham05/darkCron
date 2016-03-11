08a62e239b0cee4dd165b0b9f003f2fbc8a8643e
hm <harsham05@gmail.com>
2016-03-11 15:53:08 -0800

    add ALv2 headers

--- dark_chron.sh
+++ dark_chron.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
 yesterday=$(date -d 'yesterday' '+%Y-%m-%d')
 
 outfile=$yesterday"weap.out"
@@ -8,16 +25,9 @@ cd ~/Downloads/darkCron
 #cd /usr/local/memex/darkWeapons/chron
 
 #10000000
-python es_allSearch.py wea.txt outFiles/$outfile weapDir 100 _type $yesterday
+python es_allSearch.py wea.txt outFiles/$outfile weapDir 1000 _type $yesterday
 
-mv $outfile outFiles/
+updates=$yesterday"Updates.csv"
 
-outFiles/weaToday2.out 
-
-
-updates=$yesterday"Updates.txt"
-
-python refactoredMover.py --inCSV outFiles/$outfile --outCSV $updates --nutchDumpPath /data2/USCWeaponsStatsGathering/nutch/full_dump
-
-    
+python refactoredMover.py --inCSV outFiles/$outfile --outCSV updates/$updates --nutchDumpPath /data2/USCWeaponsStatsGathering/nutch/full_dump
 
